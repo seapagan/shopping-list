a9e604f73f429bf284dbed05919d6aa203da956f
Grant Ramsay <seapagan@gmail.com>
2022-07-24 21:31:34 +0100

    generate HTML from template

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,38 @@
 const path = require("path");
 
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
-  "mode": "none",
-  "entry": "./src/index.js",
-  "output": {
-    "path": __dirname + "/dist",
+  mode: "none",
+  entry: "./src/index.js",
+  output: {
+    path: __dirname + "/dist",
     // "filename": "bundle.[contenthash].js",
-    "filename": "bundle.js",
+    filename: "site.[contenthash].js",
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "index.html",
+    }),
+  ],
   devServer: {
     static: {
       directory: path.join(__dirname, "dist"),
     },
   },
-  "module": {
-    "rules": [
+  module: {
+    rules: [
       {
-        "test": /\.css$/,
-        "use": ["style-loader", "css-loader"],
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
       },
       {
-        "test": /\.js$/,
-        "exclude": /node_modules/,
-        "use": {
-          "loader": "babel-loader",
-          "options": {
-            "presets": ["@babel/preset-env"],
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
           },
         },
       },
