a3e629ba67d0d2fff0686c713f04e136bd0fa79b
Grant Ramsay <seapagan@gmail.com>
2022-07-25 11:19:21 +0100

    take dev/prod setting from 'mode' env
    Required converting export to a function and having the config as a
    separate object. We can then use the argv to get the mode, no longer
    needing an explicit 'NODE_ENV'

--- webpack.config.js
+++ webpack.config.js
@@ -3,48 +3,51 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-const devMode = process.env.NODE_ENV !== "production";
+module.exports = (env, argv) => {
+  const devMode = argv.mode !== "production";
 
-module.exports = {
-  mode: "development",
-  entry: "./src/index.js",
-  output: {
-    path: __dirname + "/dist",
-    // "filename": "bundle.[contenthash].js",
-    filename: "site.[contenthash].js",
-    clean: {
-      keep: /favicon/,
-    },
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "src/index.html",
-    }),
-  ].concat(devMode ? [] : [new MiniCssExtractPlugin()]),
-  devServer: {
-    static: {
-      directory: path.join(__dirname, "dist"),
-    },
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(sa|sc|c)ss$/,
-        use: [
-          devMode ? "style-loader" : MiniCssExtractPlugin.loader,
-          "css-loader",
-        ],
+  const config = {
+    mode: "development",
+    entry: "./src/index.js",
+    output: {
+      path: __dirname + "/dist",
+      filename: "site.[contenthash].js",
+      clean: {
+        keep: /favicon/,
       },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env"],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "src/index.html",
+      }),
+    ].concat(devMode ? [] : [new MiniCssExtractPlugin()]),
+    devServer: {
+      static: {
+        directory: path.join(__dirname, "dist"),
+      },
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(sa|sc|c)ss$/,
+          use: [
+            devMode ? "style-loader" : MiniCssExtractPlugin.loader,
+            "css-loader",
+          ],
+        },
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+            },
           },
         },
-      },
-    ],
-  },
+      ],
+    },
+  };
+
+  return config;
 };
