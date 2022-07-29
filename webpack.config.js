39ae08e0c47866562a8eb3381982e7d7c83dbce5
Grant Ramsay <seapagan@gmail.com>
2022-07-29 13:41:23 +0100

    update to SPBuild for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,61 @@
 const path = require("path");
+const fs=require("fs");
 
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const ESLintPlugin = require("eslint-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const StylelintPlugin = require("stylelint-webpack-plugin");
+
+
+const chooseEntry = () => {
+  // this will use an index.ts file if exists, otherwise uses index.js
+  // if both, the .ts is preferred
+  const indexTs = path.join(__dirname, "src/index.ts");
+  const indexJs = path.join(__dirname, "src/index.js");
+  return fs.existsSync(indexTs) ? indexTs : indexJs;
+};
+
+const haveFavicon = () => {
+  // if there is a favicon.ico file, use it
+  const favicon = path.join(__dirname, "src/favicon.ico");
+  return fs.existsSync(favicon);
+};
 
 module.exports = (env, argv) => {
   const devMode = argv.mode !== "production";
 
   const config = {
     mode: argv.mode ? argv.mode : "development",
-    entry: "./src/index.js",
+    entry: {
+      bundle: chooseEntry("src")
+    },
+    devtool: devMode ? "eval" : "source-map",
+    resolve: {
+      extensions: [".tsx", ".ts", ".js"],
+    },
     output: {
       path: __dirname + "/dist",
-      filename: "site.[contenthash].js",
-      clean: {
-        keep: /favicon/,
-      },
+      filename: "[contenthash].js",
+      clean: true,
     },
     plugins: [
       new HtmlWebpackPlugin({
         template: "src/index.html",
-        inject: false,
+        favicon: haveFavicon() ? "src/favicon.ico" : "",
       }),
+      new ESLintPlugin(),
+      new StylelintPlugin({
+        configFile: ".stylelintrc.json"
+      })
     ].concat(
       devMode
         ? []
         : [
-            new MiniCssExtractPlugin({
-              filename: "site.[contenthash].css",
-            }),
-          ]
+          new MiniCssExtractPlugin({
+            filename: "[contenthash].css",
+          }),
+        ]
     ),
     devServer: {
       client: {
@@ -48,11 +74,13 @@ module.exports = (env, argv) => {
           use: [
             devMode ? "style-loader" : MiniCssExtractPlugin.loader,
             "css-loader",
+            "sass-loader"
           ],
         },
         {
           test: /\.js$/,
           exclude: /node_modules/,
+          include: path.resolve(__dirname, "src"),
           use: {
             loader: "babel-loader",
             options: {
@@ -60,6 +88,13 @@ module.exports = (env, argv) => {
             },
           },
         },
+        {
+          test: /\.tsx?$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "ts-loader",
+          },
+        }
       ],
     },
     optimization: {
