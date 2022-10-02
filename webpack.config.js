5b4e1495e345ffa26b51f0d7a43d37ceb9a5187e
Grant Ramsay <seapagan@gmail.com>
2022-10-02 15:30:36 +0100

    update to sp-build v1.2.0

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 /* -------------------------------------------------------------------------- */
-/* ------------------- SPBuild Sytem Verison 1.0.0.beta.2 ------------------- */
+/* ----------------------- SPBuild Sytem Verison 1.2.0 ---------------------- */
 /* --------------- (C) Grant Ramsay 2022 under the MIT Licence -------------- */
 /* ------------------ https://github.com/seapagan/sp-build ------------------ */
 /* -------------------------------------------------------------------------- */
@@ -100,6 +100,15 @@ module.exports = (_env, argv) => {
                 },
               },
             },
+            {
+              loader: "postcss-loader",
+              options: {
+                sourceMap: true,
+                postcssOptions: {
+                  config: "postcss.config.js",
+                },
+              },
+            },
             "sass-loader",
           ],
           include: /\.module\.(sa|sc|c)ss$/,
@@ -109,6 +118,15 @@ module.exports = (_env, argv) => {
           use: [
             devMode ? "style-loader" : MiniCssExtractPlugin.loader,
             "css-loader",
+            {
+              loader: "postcss-loader",
+              options: {
+                sourceMap: true,
+                postcssOptions: {
+                  config: "postcss.config.js",
+                },
+              },
+            },
             "sass-loader",
           ],
           exclude: /\.module\.(sa|sc|c)ss$/,
