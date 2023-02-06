3934d9f2db2b8df3ff92cee1821e88b5000f0538
Grant Ramsay <seapagan@gmail.com>
2023-02-06 15:09:27 +0000

    take Supabase URL from env

--- src/modules/supabase.js
+++ src/modules/supabase.js
@@ -1,5 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://abhcrrueuovivajeyzdp.supabase.co";
+const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_KEY;
+
 export const supabase = createClient(supabaseUrl, supabaseKey);
